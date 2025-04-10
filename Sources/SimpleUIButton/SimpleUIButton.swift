8247be22cbd75282a47b06da217330607d5fd5ed
daikimochizuki <mocchi.d@gmail.com>
2025-04-10 11:42:22 -0700

    init

--- Sources/SimpleUIButton/SimpleUIButton.swift
+++ Sources/SimpleUIButton/SimpleUIButton.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
-// https://docs.swift.org/swift-book
+// https://docs.swift.org/swift-booimport Foundation
+
+import SwiftUI
+
+public struct SimpleButton: View {
+    public var title: String
+    public var action: () -> Void
+
+    public init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+
+    public var body: some View {
+        Button(action: action) {
+            Text(title)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.red)
+                .cornerRadius(8)
+        }
+    }
+}
